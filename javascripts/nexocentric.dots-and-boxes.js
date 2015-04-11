a4df9474f217b91f8fee83c93e0e9a868967e2d3
Dodzi Dzakuma <nexocentric@albion.com>
2015-04-11 07:02:24 -0500

    All clicks now register and I can determine what has been clicked. Now I just need to figure out how to determine which player owns the square.

--- javascripts/nexocentric.dots-and-boxes.js
+++ javascripts/nexocentric.dots-and-boxes.js
@@ -1,8 +1,16 @@
-var verticalMoveList = [];
-var horizontalMoveList = [];
-var squaresTaken = [];
-
+var NEUTRAL_MARKER = 0;
+var PLAYER_ONE_MARKER = 1;
+var PLAYER_TWO_MARKER = 2;
+var GAME_GRID_COLUMNS = 4;
+var GAME_GRID_ROWS = 4;
+var LINE_OFFSET = 1;
+var HORIZONTALLY = "horizontal";
+var VERTICALLY = "vertical";
+var verticalLinesClaimed = [];
+var horizontalLinesClaimed = [];
+var squaresClaimed = [];
 var turnsTaken = 0;
+var markerForCurrentPlayer = PLAYER_ONE_MARKER;
 
 //==========================================================
 // [author]
@@ -34,7 +42,7 @@ function createArray(numberOfColumns, numberOfRows) {
 		// if only columns were specified
 		//--------------------------------------
 		for (var column = 0; column < numberOfColumns; column++) {
-			createdArray[column] = 0;
+			createdArray[column] = NEUTRAL_MARKER;
 		}
 
 		return createdArray;
@@ -52,10 +60,162 @@ function createArray(numberOfColumns, numberOfRows) {
 		createdArray[newRow] = new Array(numberOfColumns);
 
 		for (var newColumn = 0; newColumn < numberOfColumns; newColumn++) {
-			createdArray[newRow][newColumn] = 0;
+			createdArray[newRow][newColumn] = NEUTRAL_MARKER;
 		}
 	}
 	return createdArray;
+}
+
+function claim(playerMarker, arrayForClaim, columnIndex, rowIndex) {
+	if (arrayForClaim[rowIndex][columnIndex] != NEUTRAL_MARKER) {
+		return false;
+	}
+
+	arrayForClaim[rowIndex][columnIndex] = playerMarker;
+	return true;
+}
+
+//==========================================================
+// [author]
+// Dodzi Y. Dzakuma
+// [summary]
+// 
+// [parameters]
+// none
+// [return]
+// none
+//==========================================================
+function countClaimsByPlayer(playerMarker, arrayToCount) {
+	var rowCount = arrayToCount.length;
+	var columnCount = arrayToCount[0].length;
+	var totalClaims = 0;
+
+	for (var row = 0; row < rowCount; row++) {
+		for (var column = 0; column < columnCount; column++) {
+			if (arrayToCount[row][column] != playerMarker) {
+				continue;
+			}
+			totalClaims += 1;
+		}
+	}
+
+	return totalClaims;
+}
+
+//==========================================================
+// [author]
+// Dodzi Y. Dzakuma
+// [summary]
+// 
+// [parameters]
+// none
+// [return]
+// none
+//==========================================================
+function resetGameCounters() {
+	verticalLinesClaimed = createArray(
+		GAME_GRID_ROWS,
+		GAME_GRID_COLUMNS + LINE_OFFSET
+	);
+	horizontalLinesClaimed = createArray(
+		GAME_GRID_ROWS + LINE_OFFSET,
+		GAME_GRID_COLUMNS
+	);
+	squaresClaimed = createArray(
+		GAME_GRID_ROWS,
+		GAME_GRID_COLUMNS
+	);
+	
+	return true;
+}
+
+function restartGame() {
+	resetGameCounters();
+}
+
+//just need to convert this for image toggling
+function replaceImage(imagePath, uploadTimestamp, jqueryObject, photoWidth, photoHeight) {
+	//--------------------------------------
+	// load the photo from its url and
+	// add it to the photowall when loaded
+	//--------------------------------------
+	var frameClass = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+	var tooltip = $('<span data-tooltip aria-haspopup="true" class="has-tip" data-options="show_on:large" title="Large Screen Sizes"></span>');
+
+	if (photoWidth > photoHeight) {
+		var img = $('<img class="photo frame-style-' + frameClass + '" width="100%" data-width="' + photoWidth + '" data-height="' + photoHeight + '" alt="SOMETHING!">').attr('src', imagePath).attr(timestampAttribute, uploadTimestamp).load(function() {
+			if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+				console.log('Image does not exist.');
+			} else {
+				$(jqueryObject).first().replaceWith(img);
+			}
+		});
+	} else {
+		var img = $('<img class="photo frame-style-' + frameClass + '" width="60%" data-width="' + photoWidth + '" data-height="' + photoHeight + '" alt="SOMETHING!">').attr('src', imagePath).attr(timestampAttribute, uploadTimestamp).load(function() {
+		if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+				console.log('Image does not exist.');
+			} else {
+				$(jqueryObject).first().replaceWith(img);
+			}
+		});
+	}
+}
+
+function resetImages() {
+
+}
+
+function resetGame() {
+
+}
+
+function gameFinished(playerOneScore, playerTwoScore) {
+
+}
+
+function displayWinner(playerMarker) {
+
+}
+
+function checkHorizontalClaims()
+{
+	
+}
+
+function checkVerticalClaims()
+{
+	
+}
+
+function determineClaim(direction, columnIndex, rowIndex) {
+	var maxHorizontalLines = horizontalLinesClaimed[0].length;
+	var maxVerticalLines = verticalLinesClaimed[0].length;
+	var topClaimed = false;
+	var bottomClaimed = false;
+	var leftClaimed = false;
+	var rightClamed = false;
+	
+	if (==) {
+		topClaimed = true;
+	}
+
+	if (==) {
+		bottomClaimed = true;
+	}
+
+	if (==) {
+		leftClaimed = true;
+	}
+
+	if (==) {
+		rightClaimed = true;
+	}
+}
+
+
+function boxClaimed(direction, columnIndex, rowIndex) {
+
+	return playerMarker;
 }
 
 //==========================================================
@@ -76,6 +236,32 @@ function connectDots(direction, columnIndex, rowIndex) {
 
 
 function run(direction, columnIndex, rowIndex) {
-	alert(direction + " " + turnsTaken);
+	var validMove = false;
+
+	if (direction == HORIZONTALLY) {
+		validMove = claim(
+			markerForCurrentPlayer,
+			horizontalLinesClaimed,
+			columnIndex,
+			rowIndex
+		)
+	} else {
+		validMove = claim(
+			markerForCurrentPlayer,
+			verticalLinesClaimed,
+			columnIndex,
+			rowIndex
+		)
+	}
+
+	if (validMove == false) {
+		return 0;
+	}
+
+
+
+	alert(validMove)
 	return 1;
 }
+
+window.onload = restartGame;
--- tests/nexocentric.dots-and-boxes-test.js
+++ tests/nexocentric.dots-and-boxes-test.js
@@ -11,3 +11,20 @@ QUnit.test("createArray function", function(assert) {
 	assert.deepEqual(returnValue.length, 4, "Returns a one dimensional array if only column numbers are passed.");
 	assert.deepEqual(returnValue[0].length, 4, "returns an empty array if no parameters passed.");
 });
+
+QUnit.test("countClaimsByPlayer function", function(assert) {
+	var testSetOne = [[1, 1], [1, 1]];
+	var testSetTwo = [[1, 2], [1, 1]];
+
+	var returnValue = countClaimsByPlayer(1, testSetOne);
+	assert.deepEqual(returnValue, 4, "returns an empty array if no parameters passed.");
+	
+	returnValue = countClaimsByPlayer(2, testSetOne);
+	assert.deepEqual(returnValue, 0, "returns an empty array if no parameters passed.");
+
+	returnValue = countClaimsByPlayer(1, testSetTwo);
+	assert.deepEqual(returnValue, 3, "returns an empty array if no parameters passed.");
+
+	returnValue = countClaimsByPlayer(2, testSetTwo);
+	assert.deepEqual(returnValue, 1, "returns an empty array if no parameters passed.");
+});
