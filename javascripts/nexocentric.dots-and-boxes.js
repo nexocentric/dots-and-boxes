85020437b6872e6e2ce5372e9b8d39f75429088b
Dodzi Dzakuma <nexocentric@albion.com>
2015-04-10 19:36:17 -0500

    I have a basic structure down. Now all I need to do is start implementing functions to use it.

---
+++ javascripts/nexocentric.dots-and-boxes.js
@@ -0,0 +1,23 @@
+var turnsTaken = 0;
+
+//==========================================================
+// [author]
+// Dodzi Y. Dzakuma
+// [summary]
+// 
+// [parameters]
+// none
+// [return]
+// none
+//==========================================================
+function connectDots(direction, columnIndex, rowIndex) {
+	//alert("Clicked.");
+	console.log("Clicked");
+	return nextPlayer;
+}
+
+
+function run(direction, columnIndex, rowIndex) {
+	alert(direction + " " + turnsTaken);
+	return 1;
+}
